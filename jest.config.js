bb785d38d83c0cd078ded98babe8b583e7c5586e
Ahmet Can Küçükkor <ahmetcankucukkor@gmail.com>
2025-09-01 00:06:45 +0300

    initial project setup with LitElement some dependencies like tailwind, jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.js'],
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/**/*.d.ts',
+    '!src/test/**/*'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
+    }
+  }
+}
